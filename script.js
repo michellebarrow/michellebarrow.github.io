899e1d6e81f92f57656aa519d226f9e2651bcdf3
michellebarrow <michellebarrow@gmail.com>
2016-04-10 15:00:37 -0400

    V 5 (end of class)+JS

---
+++ script.js
@@ -0,0 +1,80 @@
+
+// function personalFact () {
+// 	alert("I'm almost 30")
+// }
+// personalFact();
+
+// function addNumbers(a, b){
+// 	console.log(a + b)
+// }
+
+// addNumbers (3, 5)
+
+// var cars = 5;
+// var bikes = 10;
+
+// addNumbers (cars, bikes);
+
+// var name= prompt ("What's your name?")
+
+// alert ("Welcome to my site" + name);
+
+// var canDeliverToCsutomer = false;
+
+// var address = "1111 Light St"
+
+// function doWork() {
+// if (address == "1111 Light St") {canDeliverToCsutomer = true; }
+// else { canDeliverToCsutomer = false;
+// }
+	
+// doWork();
+//  if (canDeliverToCsutomer) { alert("Your food is on the way")}
+//  else { alert("Sorry we can't deliver to you")}
+
+// var temperature = 58;
+
+// if (temperature >= 50) 
+
+// {
+// 	console.log("Put on shorts");
+// } 
+
+// else if (temperature >= 38) 
+// {
+// 	console.log("Put on hat"); 
+// }
+
+// var age = 28;
+// if (age >= 16) {
+//     console.log ('Yay, you can drive!');
+// } 
+// else {
+//     console.log ('Sorry, but you have ' + (16 - age) +
+//     ' years until you can drive.');
+// }
+
+// for (var i = 1; i <= 50; i++) {console.log (i);
+// 	}
+
+window.onload = function() { var nameSwap = 
+	document.getElementByID  ( "grandma-yup");
+var visitorName = prompt("What's your name?");
+if (visitorName.length > 0) {nameSwap.innerHTML = visitorName;}
+else {nameSwap.innerHTML = "new friend"
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
